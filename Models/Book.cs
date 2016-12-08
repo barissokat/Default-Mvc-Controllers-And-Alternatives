041a490144edd2461664d4be2e780ee5374c1278
bsokat <barissokat@gmail.com>
2016-12-08 21:55:54 +0300

    Model classes defined

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace DefaultMvcControllersAndAlternatives.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        [DisplayName("Book Name")]
+        public string Name { get; set; }
+        public long ISBN { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        [DisplayName("Publication Date")]
+        public int PublicationDate { get; set; }
+        public string Price { get; set; }
+        [DisplayName("Reduced Price")]
+        public string ReducedPrice { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
