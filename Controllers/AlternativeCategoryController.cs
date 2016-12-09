24ef45ca03c72413861c20c136d4741ef9725650
bsokat <barissokat@gmail.com>
2016-12-09 21:01:20 +0300

    Alternative Category Controller updated and Views added

--- Controllers/AlternativeCategoryController.cs
+++ Controllers/AlternativeCategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DefaultMvcControllersAndAlternatives.DAL;
+using DefaultMvcControllersAndAlternatives.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,53 @@ namespace DefaultMvcControllersAndAlternatives.Controllers
 {
     public class AlternativeCategoryController : Controller
     {
+        BookContext db = new BookContext();
         // GET: AlternativeCategory
         public ActionResult Index()
         {
+            return View(db.Categories);
+        }
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return Redirect("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var category = db.Categories.Where(c => c.Id == id).FirstOrDefault();
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            Category editedCategory = db.Categories.Where(c => c.Id == category.Id).FirstOrDefault();
+            editedCategory.Name = category.Name;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var category = db.Categories.Where(c => c.Id == id).FirstOrDefault();
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            Category deletedCategory = db.Categories.Where(c => c.Id == category.Id).FirstOrDefault();
+            db.Categories.Remove(deletedCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var category = db.Categories.Where(c => c.Id == id).FirstOrDefault();
+            return View(category);
         }
     }
 }
